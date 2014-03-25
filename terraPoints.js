be80b64d8508d05059d489abccfea0ea813528e0
DeEgge <ch.kuntzsch@gmail.com>
2014-03-25 21:20:42 +0100

    All necessary files added

---
+++ terraPoints.js
@@ -0,0 +1,106 @@
+var points = [{
+"type": "FeatureCollection",
+//"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "id": 1, "properties": { "NAME": "Sir Fitz" }, "geometry": { "type": "Point", "coordinates": [ 1768.0, 4505.0 ] } }
+,
+{ "type": "Feature", "id": 2, "properties": { "NAME": "Ahola" }, "geometry": { "type": "Point", "coordinates": [ 1590.0, 5890.0 ] } }
+,
+{ "type": "Feature", "id": 3, "properties": { "NAME": "Aholas Hafen" }, "geometry": { "type": "Point", "coordinates": [ 1556.0, 5953.0 ] } }
+,
+{ "type": "Feature", "id": 4, "properties": { "NAME": "Quatros" }, "geometry": { "type": "Point", "coordinates": [ 13078.0, 7683.0 ] } }
+,
+{ "type": "Feature", "id": 5, "properties": { "NAME": "Karavana" }, "geometry": { "type": "Point", "coordinates": [ 1683.0, 7258.0 ] } }
+,
+{ "type": "Feature", "id": 6, "properties": { "NAME": "Storkholm" }, "geometry": { "type": "Point", "coordinates": [ 2302.0, 3910.0 ] } }
+,
+{ "type": "Feature", "id": 7, "properties": { "NAME": "Norfesta Wald" }, "geometry": { "type": "Point", "coordinates": [ 2295.0, 4098.0 ] } }
+,
+{ "type": "Feature", "id": 8, "properties": { "NAME": "Schloss Loire" }, "geometry": { "type": "Point", "coordinates": [ 2331.0, 5121.0 ] } }
+,
+{ "type": "Feature", "id": 9, "properties": { "NAME": "Loire" }, "geometry": { "type": "Point", "coordinates": [ 2235.0, 5347.0 ] } }
+,
+{ "type": "Feature", "id": 10, "properties": { "NAME": "Schloss Torromia" }, "geometry": { "type": "Point", "coordinates": [ 1877.0, 5701.0 ] } }
+,
+{ "type": "Feature", "id": 11, "properties": { "NAME": "Sahara" }, "geometry": { "type": "Point", "coordinates": [ 2181.0, 6706.0 ] } }
+,
+{ "type": "Feature", "id": 12, "properties": { "NAME": "Zue" }, "geometry": { "type": "Point", "coordinates": [ 2292.0, 7782.0 ] } }
+,
+{ "type": "Feature", "id": 13, "properties": { "NAME": "Schlucht" }, "geometry": { "type": "Point", "coordinates": [ 2841.0, 8914.0 ] } }
+,
+{ "type": "Feature", "id": 14, "properties": { "NAME": "Safarium" }, "geometry": { "type": "Point", "coordinates": [ 2334.0, 7321.0 ] } }
+,
+{ "type": "Feature", "id": 15, "properties": { "NAME": "Mooswald" }, "geometry": { "type": "Point", "coordinates": [ 3480.0, 3486.0 ] } }
+,
+{ "type": "Feature", "id": 16, "properties": { "NAME": "Molly" }, "geometry": { "type": "Point", "coordinates": [ 3289.0, 3465.0 ] } }
+,
+{ "type": "Feature", "id": 17, "properties": { "NAME": "Devota" }, "geometry": { "type": "Point", "coordinates": [ 4100.0, 4231.0 ] } }
+,
+{ "type": "Feature", "id": 18, "properties": { "NAME": "Berugas Labor" }, "geometry": { "type": "Point", "coordinates": [ 4057.0, 5250.0 ] } }
+,
+{ "type": "Feature", "id": 19, "properties": { "NAME": "Indus" }, "geometry": { "type": "Point", "coordinates": [ 4691.0, 6397.0 ] } }
+,
+{ "type": "Feature", "id": 20, "properties": { "NAME": "Taklama" }, "geometry": { "type": "Point", "coordinates": [ 5228.0, 4837.0 ] } }
+,
+{ "type": "Feature", "id": 21, "properties": { "NAME": "Lhasa" }, "geometry": { "type": "Point", "coordinates": [ 5330.0, 5765.0 ] } }
+,
+{ "type": "Feature", "id": 22, "properties": { "NAME": "Eklemata" }, "geometry": { "type": "Point", "coordinates": [ 5329.0, 6212.0 ] } }
+,
+{ "type": "Feature", "id": 23, "properties": { "NAME": "Drachenfels" }, "geometry": { "type": "Point", "coordinates": [ 6164.0, 6719.0 ] } }
+,
+{ "type": "Feature", "id": 24, "properties": { "NAME": "Odemfels" }, "geometry": { "type": "Point", "coordinates": [ 7067.0, 9487.0 ] } }
+,
+{ "type": "Feature", "id": 25, "properties": { "NAME": "Nomadendorf" }, "geometry": { "type": "Point", "coordinates": [ 5842.0, 4608.0 ] } }
+,
+{ "type": "Feature", "id": 26, "properties": { "NAME": "Luran" }, "geometry": { "type": "Point", "coordinates": [ 6426.0, 4949.0 ] } }
+,
+{ "type": "Feature", "id": 27, "properties": { "NAME": "Neotokio" }, "geometry": { "type": "Point", "coordinates": [ 7386.0, 6157.0 ] } }
+,
+{ "type": "Feature", "id": 28, "properties": { "NAME": "Yamei" }, "geometry": { "type": "Point", "coordinates": [ 6461.0, 6723.0 ] } }
+,
+{ "type": "Feature", "id": 29, "properties": { "NAME": "Turboschuhe" }, "geometry": { "type": "Point", "coordinates": [ 6328.0, 8097.0 ] } }
+,
+{ "type": "Feature", "id": 30, "properties": { "NAME": "Goldküste" }, "geometry": { "type": "Point", "coordinates": [ 7677.0, 9603.0 ] } }
+,
+{ "type": "Feature", "id": 31, "properties": { "NAME": "Polynesien" }, "geometry": { "type": "Point", "coordinates": [ 8720.0, 6275.0 ] } }
+,
+{ "type": "Feature", "id": 32, "properties": { "NAME": "Berugas Turm" }, "geometry": { "type": "Point", "coordinates": [ 8596.0, 3716.0 ] } }
+,
+{ "type": "Feature", "id": 33, "properties": { "NAME": "Grecliff" }, "geometry": { "type": "Point", "coordinates": [ 11603.0, 5516.0 ] } }
+,
+{ "type": "Feature", "id": 34, "properties": { "NAME": "Sanktum\/Kingnest" }, "geometry": { "type": "Point", "coordinates": [ 11284.0, 5966.0 ] } }
+,
+{ "type": "Feature", "id": 35, "properties": { "NAME": "Colorado" }, "geometry": { "type": "Point", "coordinates": [ 12118.0, 6405.0 ] } }
+,
+{ "type": "Feature", "id": 36, "properties": { "NAME": "Windlichtung" }, "geometry": { "type": "Point", "coordinates": [ 12019.0, 4647.0 ] } }
+,
+{ "type": "Feature", "id": 37, "properties": { "NAME": "Seeheim" }, "geometry": { "type": "Point", "coordinates": [ 12440.0, 5386.0 ] } }
+,
+{ "type": "Feature", "id": 38, "properties": { "NAME": "Pingea" }, "geometry": { "type": "Point", "coordinates": [ 14092.0, 3009.0 ] } }
+,
+{ "type": "Feature", "id": 39, "properties": { "NAME": "Liberita" }, "geometry": { "type": "Point", "coordinates": [ 12799.0, 6079.0 ] } }
+,
+{ "type": "Feature", "id": 40, "properties": { "NAME": "Orkania" }, "geometry": { "type": "Point", "coordinates": [ 12120.0, 5968.0 ] } }
+,
+{ "type": "Feature", "id": 41, "properties": { "NAME": "Guiana" }, "geometry": { "type": "Point", "coordinates": [ 12733.0, 8000.0 ] } }
+,
+{ "type": "Feature", "id": 42, "properties": { "NAME": "Portal" }, "geometry": { "type": "Point", "coordinates": [ 13075.0, 8585.0 ] } }
+,
+{ "type": "Feature", "id": 43, "properties": { "NAME": "Astarika" }, "geometry": { "type": "Point", "coordinates": [ 12577.0, 5894.0 ] } }
+,
+{ "type": "Feature", "id": 44, "properties": { "NAME": "Lumina" }, "geometry": { "type": "Point", "coordinates": [ 13331.0, 9026.0 ] } }
+,
+{ "type": "Feature", "id": 45, "properties": { "NAME": "Liotto" }, "geometry": { "type": "Point", "coordinates": [ 13957.0, 9474.0 ] } }
+,
+{ "type": "Feature", "id": 46, "properties": { "NAME": "Mu" }, "geometry": { "type": "Point", "coordinates": [ 10024.0, 9347.0 ] } }
+,
+{ "type": "Feature", "id": 47, "properties": { "NAME": "Vogel nach Grönland" }, "geometry": { "type": "Point", "coordinates": [ 13138.0, 10116.0 ] } }
+,
+{ "type": "Feature", "id": 48, "properties": { "NAME": "Dürretal" }, "geometry": { "type": "Point", "coordinates": [ 8841.0, 14531.0 ] } }
+,
+{ "type": "Feature", "id": 49, "properties": { "NAME": "Höhle" }, "geometry": { "type": "Point", "coordinates": [ 12694.0, 5139.0 ] } }
+
+]
+}
+]
---
+++ workflow.R
@@ -0,0 +1,20 @@
+setwd("E:\\Eigene Dokumente\\Geodaten\\Terranigma")
+
+# Packages installieren
+#install.packages("devtools")
+#install.packages("ggmap")
+#install.packages("sp")
+#install.packages("rgdal")
+#require(devtools)
+require(ggmap)
+require(sp)
+require(rgdal)
+#install_github("rCharts","ramnathv")
+require(rCharts)
+
+# Rohdaten laden
+points = read.csv("tp.csv", header=TRUE, sep=";", encoding="latin1", stringsAsFactors=FALSE)
+
+# geoJSON erzeugen
+pointsSP = SpatialPointsDataFrame(points[,c(3,4)], as.data.frame(points[,2]))  # erst ein SP-Format erzeugen
+writeOGR(pointsSP, "terraPoints.js", "points", driver="GeoJSON")
