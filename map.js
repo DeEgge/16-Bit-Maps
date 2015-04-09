e9748774c2b3b82ba5a337346779c2bd970d1cd0
DeEgge <ch.kuntzsch@gmail.com>
2015-04-09 17:36:27 +0200

    more jquery

--- map.js
+++ map.js
@@ -1,32 +1,23 @@
-/*var _overworld,_underworld
+var _overworld,_underworld
 
 function init() {
-	$.get('data/overworld.geojson', function(json) {
-		_overworld = json;
-		$.get('data/underworld.geojson', function(json) {
-			_underworld = json;
-			$.ajax({
-				url:'data/all.json',
-				type: 'GET',
-				dataType: 'json',
-				cache: false,
-				success: function (json) {
-					_quality = json;
-					initMap();
-				}
-			});
+	$.get("data/underworld.geojson", function(json){
+		_underworld = json;
+		$.get("data/overworld.geojson", function(json){
+			_overworld = json;
+			initMap();
 		});
 	});
-}*/
+}
 
 function initMap() {
 
-		var overworld = L.tileLayer('overworldTiles/{z}/{x}/{y}.png', {
+		var overworldBasemap = L.tileLayer('overworldTiles/{z}/{x}/{y}.png', {
 	        tms: true,
             attribution: 'Imagery and data: <a href="http://www.nintendo.com">NINTENDO</a>'
 		});
 		
-		var underworld = L.tileLayer('underworldTiles/{z}/{x}/{y}.png', {
+		var underworldBasemap = L.tileLayer('underworldTiles/{z}/{x}/{y}.png', {
             tms: true,
             attribution: 'Imagery and data: <a href="http://www.nintendo.com">NINTENDO</a>'
 		});
@@ -34,14 +25,83 @@ function initMap() {
 		var map = L.map('map', {
 			maxZoom: 6,
 			minZoom: 2,
-			layers: [overworld, underworld],
+			layers: [overworldBasemap, underworldBasemap],
 			crs: L.CRS.Simple
 		}).setView([0, 0], 2);
         var southWest = map.unproject([0,16384], map.getMaxZoom());
 		var northEast = map.unproject([16384,0], map.getMaxZoom());
 		map.setMaxBounds(new L.LatLngBounds(southWest, northEast));
 
-		var overworldPoints = new L.geoJson(overworldPoints, {
+		var icons = {
+			'village': L.icon({
+				iconUrl: 'img/village.png',
+				iconSize: [60,64], // size of the icon width,height
+				iconAnchor: [30,32], // point of the icon which will correspond to marker's location
+				popupAnchor: [30,32] // point from which the popup should open relative to the iconAnchor
+			}),
+			'dungeon': L.icon({
+				iconUrl: 'img/dungeon.png',
+				iconSize: [64,55], 
+				iconAnchor: [32,27], 
+				popupAnchor: [32,27] 
+			})
+		};
+
+		/*var overlay = {
+			'Unterwelt': new L.layerGroup(),
+			'Oberwelt': new L.layerGroup()
+		};*/
+
+		/*L.control.layers({}, overlay, {collapsed: false}).addTo(map);*/
+
+		//Unterwelt
+		var underworldPoints;
+		var underworldGroup = new L.layerGroup();
+		$.getJSON("data/underworld.geojson",function(data){
+   			// add GeoJSON layer to the map once the file is loaded
+    		var underworldPoints = L.geoJson(data,{
+    			coordsToLatLng: function (newcoords) {
+					return (map.unproject([newcoords[0], newcoords[1]], map.getMaxZoom()));
+				},
+				pointToLayer: function (feature, coords) {
+				return L.marker(coords);
+				},
+				onEachFeature: function (feature, layer) {
+					layer.bindPopup(feature.properties.NAME);
+				}
+			});
+			var underworldGroup = L.layerGroup([underworldBasemap, underworldPoints]).addTo(map);
+		});
+
+		//Oberwelt
+		var overworldPoints;
+		var overworldGroup = new L.layerGroup();
+		$.getJSON("data/overworld.geojson",function(data){
+   			// add GeoJSON layer to the map once the file is loaded
+    		var overworldPoints = L.geoJson(data,{
+    			coordsToLatLng: function (newcoords) {
+					return (map.unproject([newcoords[0], newcoords[1]], map.getMaxZoom()));
+				},
+				pointToLayer: function (feature, coords) {
+				return L.marker(coords);
+				},
+				onEachFeature: function (feature, layer) {
+					layer.bindPopup(feature.properties.NAME);
+				}
+			});
+			var overworldGroup = L.layerGroup([overworldBasemap, overworldPoints]);
+		});
+
+		var layers = {
+			"Oberwelt": overworldGroup,
+			"Unterwelt": underworldGroup
+		};
+
+		L.control.layers(layers, null, {
+			collapsed: false
+		}).addTo(map);
+
+		/*var overworldPoints = new L.geoJson(overworldPoints, {
 			//convert pixel coordinates to global coordinates
 			coordsToLatLng: function (newcoords) {
 				return (map.unproject([newcoords[0], newcoords[1]], map.getMaxZoom()));
@@ -75,11 +135,11 @@ function initMap() {
 		var underworldGroup = L.layerGroup([underworld, underworldPoints]).addTo(map);
 
 		var layers = {
-			"Oberwelt": overworldGroup,
-			"Unterwelt": underworldGroup
+			"Oberwelt": overworld,
+			"Unterwelt": underworld
 		};
 
 		L.control.layers(layers, null, {
 			collapsed: false
-		}).addTo(map);
+		}).addTo(map);*/
 }
